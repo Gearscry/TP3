4715249f74923b238285e8b3bcdaebedbab55521
Sebastien <sebastien.demousselle@gmail.com>
2020-03-20 15:00:16 +0100

    tp3

--- cmake-build-debug/CMakeFiles/TP3.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/TP3.dir/cmake_clean.cmake
@@ -1,11 +1,13 @@
 file(REMOVE_RECURSE
-  "TP3_automoc.cpp"
+  "CMakeFiles/TP3_autogen.dir/AutogenOldSettings.txt"
+  "CMakeFiles/TP3_autogen.dir/ParseCache.txt"
+  "TP3_autogen"
+  "CMakeFiles/TP3.dir/TP3_autogen/mocs_compilation.cpp.o"
   "CMakeFiles/TP3.dir/main.cpp.o"
-  "CMakeFiles/TP3.dir/mainWindow_Exo2.cpp.o"
   "CMakeFiles/TP3.dir/mainWindow_Exo1.cpp.o"
-  "CMakeFiles/TP3.dir/TP3_automoc.cpp.o"
-  "TP3.pdb"
+  "CMakeFiles/TP3.dir/mainWindow_Exo2.cpp.o"
   "TP3"
+  "TP3.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
--- cmake-build-debug/CMakeFiles/TP3_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "TP3_automoc.cpp"
-  "CMakeFiles/TP3_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/TP3_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
